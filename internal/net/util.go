b0c03d80e2ac9ee2e51ffa8d312bda61f582c5fb
API2(云盘) <164052842+long2005a1@users.noreply.github.com>
2025-02-03 07:22:00 +0000

    feat（别名）：add 和 option （#7829）DownloadConcurrencyDownloadPartSize)

--- internal/net/util.go
+++ internal/net/util.go
@@ -2,7 +2,6 @@ package net
 
 import (
 	"fmt"
-	"github.com/alist-org/alist/v3/pkg/utils"
 	"io"
 	"math"
 	"mime/multipart"
@@ -11,6 +10,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/alist-org/alist/v3/pkg/utils"
 	"github.com/alist-org/alist/v3/pkg/http_range"
 	log "github.com/sirupsen/logrus"
 )
