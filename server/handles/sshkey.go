6dcb43035ca8dfac129255f0f4ee9f5d4d92cf25
API2(云盘) <164052842+long2005a1@users.noreply.github.com>
2025-03-28 05:28:01 +0000

    修复 （sftp-server）：postgre 无法存储控制字符

--- server/handles/sshkey.go
+++ server/handles/sshkey.go
@@ -1,12 +1,12 @@
 package handles
 
 import (
-	"strconv"
-
 	"github.com/alist-org/alist/v3/internal/model"
 	"github.com/alist-org/alist/v3/internal/op"
 	"github.com/alist-org/alist/v3/server/common"
 	"github.com/gin-gonic/gin"
+	"strconv"
+	"strings"
 )
 
 type SSHKeyAddReq struct {
@@ -31,7 +31,7 @@ func AddMyPublicKey(c *gin.Context) {
 	}
 	key := &model.SSHPublicKey{
 		Title:  req.Title,
-		KeyStr: req.Key,
+		KeyStr: strings.TrimSpace(req.Key),
 		UserId: userObj.ID,
 	}
 	err, parsed := op.CreateSSHPublicKey(key)
